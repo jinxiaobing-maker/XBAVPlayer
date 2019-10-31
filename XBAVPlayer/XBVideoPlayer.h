18c89ec8430f8435748f8caa0990c2041d5e73d8
youxiao <984013284@qq.com>
2019-10-31 13:02:03 +0800

    first commit

---
+++ XBAVPlayer/XBVideoPlayer.h
@@ -0,0 +1,17 @@
+//
+//  XBVideoPlayer.h
+//  XBAVPlayer
+//
+//  Created by youxiao on 2019/10/31.
+//  Copyright © 2019 youxiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XBVideoPlayer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XBAVPlayer/XBVideoPlayer.m
@@ -0,0 +1,13 @@
+//
+//  XBVideoPlayer.m
+//  XBAVPlayer
+//
+//  Created by youxiao on 2019/10/31.
+//  Copyright © 2019 youxiao. All rights reserved.
+//
+
+#import "XBVideoPlayer.h"
+
+@implementation XBVideoPlayer
+
+@end
