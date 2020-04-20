802dadeacea4a0ef4d4b5820d0323013879b835e
everyStudy_Now <984013284@qq.com>
2020-04-20 09:51:05 +0800

    develop 修改了同一个地方代码

--- XBAVPlayer/ViewController.m
+++ XBAVPlayer/ViewController.m
@@ -21,6 +21,16 @@
     // Do any additional setup after loading the view.
     [self initPlayer];
 }
+- (void)addTestBtn{
+    UIButton *loginBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    loginBtn.frame = CGRectMake(100, 100, 100, 100);
+    [loginBtn setTitle:@"注册吧老兄" forState:UIControlStateNormal];
+    [loginBtn addTarget:self action:@selector(registerBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:loginBtn];
+}
+- (void)registerBtnClick:(UIButton *)sender{
+    NSLog(@"这就是点击注册吧老兄btn");
+}
 - (void)initPlayer{
     NSString *testUrl = @"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
     UIView *itemView = [[UIView alloc]initWithFrame:CGRectMake(0, 100, W, 220)];
