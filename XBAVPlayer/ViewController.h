0fd06811309f3c89131914b63972a429d8b8e36a
everyStudyNow <984013284@qq.com>
2020-05-06 11:07:13 +0800

    测试提交

--- XBAVPlayer/ViewController.h
+++ XBAVPlayer/ViewController.h
@@ -5,11 +5,11 @@
 //  Created by youxiao on 2019/10/31.
 //  Copyright © 2019 youxiao. All rights reserved.
 //
-
+typedef void(^testBlock)(void);
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 
-
+@property(nonatomic, copy)testBlock block;
 @end
 
