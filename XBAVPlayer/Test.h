b8f9a0734a737ecb5954ca47ad9293f5bce6c625
xiaobing.jin <xiaobing.jin@geely.com>
2020-08-20 09:36:03 +0800

    生成一个commit

---
+++ XBAVPlayer/Test.h
@@ -0,0 +1,17 @@
+//
+//  Test.h
+//  XBAVPlayer
+//
+//  Created by xiaobing on 2020/8/20.
+//  Copyright © 2020 youxiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XBAVPlayer/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  XBAVPlayer
+//
+//  Created by xiaobing on 2020/8/20.
+//  Copyright © 2020 youxiao. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
